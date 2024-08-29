718913f78737eb0e480991f1435d9f6e6f49da04
Dmitriy-Shat <jaren@inbox.ru>
2024-08-29 21:37:00 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,24 @@
+class House:
+    house_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.house_history.append(args[0])
+        return cls.house_history
+
+    def __init__(self, name, number):
+        self.name = name
+        self.number_of_floors = number
+
+    #def __del__(self):                                                # совместно c __new__ не работает
+        #print(f'{self.name}, дом снесен, но он останется в истории')
+
+
+Korp1 = House('Волга', 15)
+Korp2 = House('Долг', 7)
+Korp3 = House('Династия', 14)
+#del Korp1
+#input()
+#del Korp2
+#input()
+
+print(House.house_history)
